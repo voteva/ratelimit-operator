9c4511e5c4859582a91715c6f17904f81e0942a9
voteva <tatianavoteva@gmail.com>
2020-07-31 13:50:36 +0500

    Fix ratelimiter image

--- pkg/constants/images.go
+++ pkg/constants/images.go
@@ -1,6 +1,6 @@
 package constants
 
 const (
-	RATE_LIMITER_SERVICE_IMAGE = "evil26r/service_rite_limit"
+	RATE_LIMITER_SERVICE_IMAGE = "evil26r/rate-limit-rhel7"
 	REDIS_IMAGE                = "redis:6.0.6"
 )
