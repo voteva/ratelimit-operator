c52c65928054779fe60e82207026e24a9a642531
voteva <tatianavoteva@gmail.com>
2020-08-28 14:34:39 +0300

    Add CRD validation

--- pkg/apis/operators/v1/ratelimiterconfig_types.go
+++ pkg/apis/operators/v1/ratelimiterconfig_types.go
@@ -8,29 +8,41 @@ type ApplyTo string
 
 const (
 	GATEWAY          ApplyTo = "GATEWAY"
-	SIDECAR_OUTBOUND ApplyTo = "SIDECAR_OUTBOUND"
 	SIDECAR_INBOUND  ApplyTo = "SIDECAR_INBOUND"
+	SIDECAR_OUTBOUND ApplyTo = "SIDECAR_OUTBOUND"
 )
 
 type RateLimit struct {
-	Unit            string `json:"unit" yaml:"unit"`
-	RequestsPerUnit int32  `json:"requests_per_unit" yaml:"requests_per_unit"`
+	// +kubebuilder:validation:Enum={second,minute,hour,day}
+	Unit string `json:"unit" yaml:"unit"`
+	// +kubebuilder:validation:Minimum=0
+	RequestsPerUnit int32 `json:"requests_per_unit" yaml:"requests_per_unit"`
 }
 
 type DescriptorInternal struct {
-	Key       string    `json:"key" yaml:"key"`
+	// +kubebuilder:validation:MaxLength=255
+	// +kubebuilder:validation:MinLength=1
+	Key string `json:"key" yaml:"key"`
+	// +kubebuilder:validation:MaxLength=255
+	// +kubebuilder:validation:MinLength=1
 	Value     string    `json:"value,omitempty" yaml:"value,omitempty"`
 	RateLimit RateLimit `json:"rate_limit,omitempty" yaml:"rate_limit,omitempty"`
 }
 
 type Descriptor struct {
-	Key         string               `json:"key" yaml:"key"`
+	// +kubebuilder:validation:MaxLength=255
+	// +kubebuilder:validation:MinLength=1
+	Key string `json:"key" yaml:"key"`
+	// +kubebuilder:validation:MaxLength=255
+	// +kubebuilder:validation:MinLength=1
 	Value       string               `json:"value,omitempty" yaml:"value,omitempty"`
 	RateLimit   RateLimit            `json:"rate_limit,omitempty" yaml:"rate_limit,omitempty"`
 	Descriptors []DescriptorInternal `json:"descriptors,omitempty" yaml:"descriptors,omitempty"`
 }
 
 type RateLimitProperty struct {
+	// +kubebuilder:validation:MaxLength=255
+	// +kubebuilder:validation:MinLength=4
 	Domain      string       `json:"domain" yaml:"domain"`
 	Descriptors []Descriptor `json:"descriptors,omitempty" yaml:"descriptors,omitempty"`
 }
@@ -40,14 +52,22 @@ type WorkloadSelector struct {
 }
 
 type RateLimiterConfigSpec struct {
-	ApplyTo                 ApplyTo           `json:"applyTo"`
-	WorkloadSelector        WorkloadSelector  `json:"workloadSelector"`
-	Port                    int32             `json:"port"`
-	Host                    *string           `json:"host,omitempty"`
-	RateLimiter             string            `json:"rateLimiter"`
-	RateLimitProperty       RateLimitProperty `json:"rateLimitProperty,omitempty"`
+	// +kubebuilder:validation:Enum={GATEWAY,SIDECAR_INBOUND,SIDECAR_OUTBOUND}
+	ApplyTo ApplyTo `json:"applyTo"`
+	// +kubebuilder:validation:MaxLength=255
+	// +kubebuilder:validation:MinLength=1
+	Host *string `json:"host,omitempty"`
+	// +kubebuilder:validation:Maximum=65535
+	// +kubebuilder:validation:Minimum=0
+	Port int32 `json:"port"`
+	// +kubebuilder:validation:MaxLength=255
+	// +kubebuilder:validation:MinLength=1
+	RateLimiter string `json:"rateLimiter"`
+	// +kubebuilder:validation:Pattern="^([0-9]+(\\.[0-9]+)?(ms|s|m|h))+$"
 	RateLimitRequestTimeout *string           `json:"rateLimitRequestTimeout,omitempty"`
+	RateLimitProperty       RateLimitProperty `json:"rateLimitProperty,omitempty"`
 	FailureModeDeny         *bool             `json:"failureModeDeny,omitempty"`
+	WorkloadSelector        WorkloadSelector  `json:"workloadSelector"`
 }
 
 type RateLimiterConfigStatus struct {
