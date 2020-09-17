bdac70412d8a0b0cadc95b50818cc3ab728c1d2d
voteva <tatianavoteva@gmail.com>
2020-09-17 11:02:21 +0300

    Fix build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 export IMAGE=voteva/ratelimit-operator:v0.0.0
 echo $IMAGE
 
-echo "operator-sdk build ..."
 go test ./pkg/... && operator-sdk build $IMAGE && echo "docker push ..." && docker push $IMAGE
